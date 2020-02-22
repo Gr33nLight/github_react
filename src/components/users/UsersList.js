ee541d6f06881fd43809b17604f760b63731f397
Gr33nLight <bdepaula520@gmail.com>
2020-02-22 23:42:29 +0100

    refactor(UserList): converted to function

--- src/components/users/UsersList.js
+++ src/components/users/UsersList.js
@@ -1,34 +1,31 @@
-import React, { Component } from 'react';
+import React from 'react';
 import UserItem from './UserItem';
 import Spinner from '../Spinner';
 import PropTypes from 'prop-types';
 import '../../list_style_fix.css';
 
-export class UsersList extends Component {
-
-  render() {
-    if (this.props.loading) {
-      return <Spinner />;
-    } else {
-      return (
-        <React.Fragment>
-          <div
-            className="list"
-            style={{
-              display: 'flex',
-              flexDirection: 'row',
-              flexWrap: 'wrap',
-              justifyItems: 'center',
-              justifyContent: 'space-between'
-            }}
-          >
-            {this.props.users.map((ele) => {
-              return <UserItem item={ele} key={ele.login} />;
-            })}
-          </div>
-        </React.Fragment>
-      );
-    }
+function UsersList({ loading, users }) {
+  if (loading) {
+    return <Spinner />;
+  } else {
+    return (
+      <React.Fragment>
+        <div
+          className="list"
+          style={{
+            display: 'flex',
+            flexDirection: 'row',
+            flexWrap: 'wrap',
+            justifyItems: 'center',
+            justifyContent: 'space-between'
+          }}
+        >
+          {users.map((ele) => {
+            return <UserItem item={ele} key={ele.login} />;
+          })}
+        </div>
+      </React.Fragment>
+    );
   }
 }
 
@@ -36,4 +33,5 @@ UsersList.propTypes = {
   users: PropTypes.array.isRequired,
   loading: PropTypes.bool.isRequired
 };
+
 export default UsersList;
